d0567ca29fc4cf607a9bc15323415ab56397b93d
Jon0945 <john.gullne@gmail.com>
2024-05-29 10:49:13 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/g2workshopspringboot/G2WorkshopSpringBootApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g2workshopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2WorkshopSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2WorkshopSpringBootApplication.class, args);
+    }
+
+}
